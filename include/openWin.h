2fdbf4b340f37003e6cb319d5b21c5dfe403416f
huanhuanonly <3347484963@qq.com>
2024-12-24 16:59:02 +0800

    feat: create openWin.h to include all headers

---
+++ include/openWin.h
@@ -0,0 +1,24 @@
+/**
+* openWin.h In the openWin (https://github.com/huanhuanonly/openWin)
+*
+* Created by Yang Huanhuan on December 24, 2024, 16:39:50
+*
+* Email -> 3347484963@qq.com
+*
+* --- This file is a part of openWin ---
+*
+* @brief All headers in the openWin.
+*/
+
+#pragma once
+
+#ifndef OPEN_WIN_H
+#define OPEN_WIN_H
+
+#include "Win.h"
+#include "Cur.h"
+
+#include "pg/Direct.h"
+#include "pg/Linear.h"
+
+#endif  // OPEN_WIN_H
